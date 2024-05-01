fe244d8f4f42cc12ac161732997001ee18486dbd
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-05-01 20:09:04 +0530

    s

---
+++ Variable/variable.js
@@ -0,0 +1,49 @@
+/*
+// Var 
+var a;
+console.log(a);
+
+var a = 20;
+console.log(a);
+
+a = 30;
+console.log(a);
+
+var a = 50;
+console.log(a);
+
+*/
+
+
+/*
+
+// let 
+let b;
+console.log(b);
+
+// let b = 20;
+// console.log(b);
+
+b = 30;
+console.log(b);
+
+// let b = 50;
+// console.log(a);
+
+
+*/
+
+// const
+
+// const c;
+// console.log(a);
+
+const c = 20;
+console.log(c);
+
+// c = 30;
+// console.log(c);
+
+// const c = 50;
+// console.log(c);
+
