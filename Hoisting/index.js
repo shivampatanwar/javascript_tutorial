2026d0d9ea57375e88caa57c0b4d2e08af795e97
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-05-02 19:03:43 +0530

    s

---
+++ Hoisting/index.js
@@ -0,0 +1,12 @@
+
+console.log(a);  // utilizing the variable   (undfined * 2-> NaN)
+var a = 10;  // initialization line
+
+
+// Hoisting is not possible for let
+// console.log(b);
+// let b = 30;
+
+// Hoisting is not possible for const
+console.log(c);
+const c = 30;
