ea31518c0a990fd8edd45b295d7973d56132ff59
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-05-05 20:20:10 +0530

    s

---
+++ Literals/Primitive/number.js
@@ -0,0 +1,17 @@
+
+// number  -1, 0, +1, 1.5
+// typeof  operator
+
+// let a = -1;
+// let b = 0;
+// let c = 1;
+// let d = 1.5;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+
+let d1 = 48;
+console.log(typeof d1);
---
+++ Literals/Primitive/string.js
@@ -0,0 +1,30 @@
+// string 
+// typeof operator
+
+// let a = "Hello's";  
+// let a = '"Hello"';
+// console.log(typeof a);
+
+
+
+// 1. enter
+// 2. space -> followed by both
+
+// let a  = "Hello   JavaScript";
+// let a1 =`Hello   
+// JavaScript
+// JavaScript1`;
+// console.log( a);
+// console.log( a1);
+
+
+
+let a = 30;
+let b = 20;
+let c = a+b;
+
+//console.log(c);   // 10 + 20 = 30
+console.log(a + " + " + b + " = " + c);   // 10 + 20 = 30
+console.log(`${a} + ${b} = ${c}`);
+
+console.log(`${a}`);
